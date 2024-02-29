d59c4fa1ba94e3eeec53e97fe07f5f94dafae875
Davidct <97263791+davidct95@users.noreply.github.com>
2024-02-29 12:25:21 -0500

    page-ng

---
+++ src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, productsList } from '../products/products.mock';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit{
+
+  product?:Product;
+  productList: Product[] = productsList;
+  loading: boolean = true;
+  color:string = '';
+
+  constructor(private _route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this._route.params.subscribe(params => {
+        this.product = this.productList.find(product => product.id == params['productId']);
+        this.color = this.product?.price as number > 5 ? 'red' : '';
+        this.loading = false;
+      });
+    }, 1500);
+  }
+}
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { productsList } from './products.mock';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+  productsList = productsList;
+}
